b5f058218d4aa6c010c0ddd29850f321df0b4a6c
Aryan Gorwade <Aryan Gorwade>
2025-08-21 16:28:09 -0700

    r

--- pkg/cmd/get/get_nimcache.go
+++ pkg/cmd/get/get_nimcache.go
@@ -1,8 +1,6 @@
 package get
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"io"
 	"time"
@@ -18,23 +16,8 @@ import (
 	appsv1alpha1 "github.com/NVIDIA/k8s-nim-operator/api/apps/v1alpha1"
 )
 
-type GetNIMCacheOptions struct {
-	cmdFactory    cmdutil.Factory
-	ioStreams     *genericclioptions.IOStreams
-	namespace     string
-	nimcache      string
-	allNamespaces bool
-}
-
-func NewGetNIMCacheOptions(cmdFactory cmdutil.Factory, streams genericclioptions.IOStreams) *GetNIMCacheOptions {
-	return &GetNIMCacheOptions{
-		cmdFactory: cmdFactory,
-		ioStreams:  &streams,
-	}
-}
-
 func NewGetNIMCacheCommand(cmdFactory cmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
-	options := NewGetNIMCacheOptions(cmdFactory, streams)
+	options := NewGetResourceOptions(cmdFactory, streams)
 
 	cmd := &cobra.Command{
 		Use:               "nimcache [NAME]",
@@ -44,7 +27,7 @@ func NewGetNIMCacheCommand(cmdFactory cmdutil.Factory, streams genericclioptions
 		// ValidArgsFunction: completion.RayClusterCompletionFunc(cmdFactory),
 		Args:              cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := options.Complete(args, cmd); err != nil {
+			if err := options.CompleteNamespace(args, cmd); err != nil {
 				return err
 			}
 			// running cmd.Execute or cmd.ExecuteE sets the context, which will be done by root
@@ -52,74 +35,11 @@ func NewGetNIMCacheCommand(cmdFactory cmdutil.Factory, streams genericclioptions
 			if err != nil {
 				return fmt.Errorf("failed to create client: %w", err)
 			}
-			return options.Run(cmd.Context(), k8sClient)
+			return options.Run(cmd.Context(), k8sClient, &appsv1alpha1.NIMCacheList{})
 		},
 	}
 	cmd.Flags().BoolVarP(&options.allNamespaces, "all-namespaces", "A", false, "If present, list the requested NIMCaches across all namespaces. Namespace in current context is ignored even if specified with --namespace.")
 	return cmd
-}
-
-// Populates GetNIMCacheOptions with namespace and nimcache name (if present).
-func (options *GetNIMCacheOptions) Complete(args []string, cmd *cobra.Command) error {
-	namespace, err := cmd.Flags().GetString("namespace")
-	if err != nil {
-		return fmt.Errorf("failed to get namespace: %w", err)
-	}
-	options.namespace = namespace
-	if options.namespace == "" {
-		options.namespace = "default"
-	}
-
-	if len(args) >= 1 {
-		options.nimcache = args[0]
-	}
-
-	return nil
-}
-
-func (options *GetNIMCacheOptions) Run(ctx context.Context, k8sClient client.Client) error {
-	rayclusterList, err := getNIMCaches(ctx, options, k8sClient)
-	if err != nil {
-		return err
-	}
-
-	return printNIMCaches(rayclusterList, options.ioStreams.Out)
-}
-
-func getNIMCaches(ctx context.Context, options *GetNIMCacheOptions, k8sClient client.Client) (*appsv1alpha1.NIMCacheList, error) {
-	var nimCacheList *appsv1alpha1.NIMCacheList
-	var err error
-
-	listopts := v1.ListOptions{}
-	if options.nimcache != "" {
-		listopts = v1.ListOptions{
-			FieldSelector: fmt.Sprintf("metadata.name=%s", options.nimcache),
-		}
-	}
-
-	if options.allNamespaces {
-		nimCacheList, err = k8sClient.NIMClient().AppsV1alpha1().NIMCaches("").List(ctx, listopts)
-		if err != nil {
-			return nil, fmt.Errorf("unable to retrieve NIMCaches for all namespaces: %w", err)
-		}
-	} else {
-		nimCacheList, err = k8sClient.NIMClient().AppsV1alpha1().NIMCaches(options.namespace).List(ctx, listopts)
-		if err != nil {
-			return nil, fmt.Errorf("unable to retrieve NIMCaches for namespace %s: %w", options.namespace, err)
-		}
-	}
-
-	if options.nimcache != "" && len(nimCacheList.Items) == 0 {
-		errMsg := fmt.Sprintf("NIMCache %s not found", options.nimcache)
-		if options.allNamespaces {
-			errMsg += " in any namespace"
-		} else {
-			errMsg += fmt.Sprintf(" in namespace %s", options.namespace)
-		}
-		return nil, errors.New(errMsg)
-	}
-
-	return nimCacheList, nil
 }
 
 func printNIMCaches(nimCacheList *appsv1alpha1.NIMCacheList, output io.Writer) error {
@@ -144,13 +64,7 @@ func printNIMCaches(nimCacheList *appsv1alpha1.NIMCacheList, output io.Writer) e
 		if nimcache.GetCreationTimestamp().Time.IsZero() {
 			age = "<unknown>"
 		}
-		/*
-		relevantConditionType := ""
-		relevantCondition := util.RelevantRayClusterCondition(raycluster)
-		if relevantCondition != nil {
-			relevantConditionType = relevantCondition.Type
-		}
-		*/
+
 		resTable.Rows = append(resTable.Rows, v1.TableRow{
 			Cells: []interface{}{
 				nimcache.GetName(),
@@ -202,9 +116,5 @@ func getModel(nimCache *appsv1alpha1.NIMCache) string {
 }
 
 func getPVCDetails(nimCache *appsv1alpha1.NIMCache) string {
-	return fmt.Sprintf("%-20s %s", nimCache.Spec.Storage.PVC.Name, nimCache.Spec.Storage.PVC.Size)
-}
-
-func getStateDetails(nimCache *appsv1alpha1.NIMCache) string {
-	return fmt.Sprintf("%-20s %s", nimCache.Spec.Storage.PVC.Name, nimCache.Spec.Storage.PVC.Size)
+	return fmt.Sprintf("%s, %s", nimCache.Spec.Storage.PVC.Name, nimCache.Spec.Storage.PVC.Size)
 }
